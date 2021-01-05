d3717027b7732ed73f866747bafb3f189dc8e285
김기홍 <68697807+KimGiHong@users.noreply.github.com>
2021-01-05 19:55:39 +0900

    study class of js
    stduy class

--- fastCampus/class.js
+++ fastCampus/class.js
@@ -90,34 +90,139 @@
 // new B().hello();
 
 //get,set 게터 세터
-class A {
-    _name = 'no name';
+// class A {
+//     _name = 'no name';
 
-    get name() {
-        return this._name + '@@@';
-    }
-    set name(value) {
-        this._name = value + '!!!';
-    }
-}
-const a = new A();
-console.log(a);
-a.name = 'Hong';
-console.log(a);
-console.log(a.name);
-console.log(a._name);
+//     get name() {
+//         return this._name + '@@@';
+//     }
+//     set name(value) {
+//         this._name = value + '!!!';
+//     }
+// }
+// const a = new A();
+// console.log(a);
+// a.name = 'Hong';
+// console.log(a);
+// console.log(a.name);
+// console.log(a._name);
 
 // readonly
 
-class B {
-    _name = 'no name';
+// class B {
+//     _name = 'no name';
 
-    get name() {
-        return this._name + '@@@';
-    }
-}
+// class A {
+//     static age = 18;
+//     static hello() {
+//         console.log(A.age);
+//     }
+// }
+// console.log(A,A.age);
+// A.hello();
 
-const b = new B();
-console.log(b);
-b.name = 'Hong';
-console.log(b);
+// class B {
+//     age = 18;
+//     static hello() {
+//         console.log(this.age);
+//     }
+// }
+// console.log(B,B.age);
+// B.hello();
+//new B().hello(); //static함수이기때문에 객체에 속해있는 함수가 아니여서 에러가 남
+
+// class C {
+//     static name = '이 클래스의 이름을 C가 아니다.'
+// }
+
+// console.log(C);
+
+//상속 기본 extends
+
+// class Parent {
+//     name = 'KIM';
+
+//     hello(){
+//         console.log('hello' , this.name);
+//     }
+// }
+
+// class Child extends Parent {
+
+// }
+
+// const p = new Parent();
+// const c = new Child();
+// console.log(p,c);
+
+// c.hello();
+// c.name = 'Hong';
+// c.hello();
+
+//override 클래스의 상속 멤버 변수 및 함수 오버라이딩,추가
+
+// class Parent{
+//     name = 'KIM'
+
+//     hello() {
+//         console.log('hello',this.name);
+//     }
+// }
+
+// class Child extends Parent{
+//     age = 18;
+
+//     hello() {
+//         console.log('hello',this.name,this.age);
+//     }
+// }
+
+// const p = new Parent();
+// const c = new Child();
+// console.log(p,c);
+// c.hello();
+// c.name = 'Hong';
+// c.hello();
+
+//super 클래스의 상속 생성자 함수 변경
+
+// class Parent {
+//     name;
+
+//     constructor(name) {
+//         this.name = name;
+//     }
+//     hello() {
+//         console.log('hello',this.name);
+//         }
+// }
+
+// class Child extends Parent{
+//     age;
+
+//     constructor(name,age) {
+//         super(name);
+//         this.age = age;
+//     }
+
+//     hello() {
+//         console.log('hello',this.name,this.age);
+//     }
+// }
+
+// const p = new Parent('Mark');
+// const c = new Child('Mark',18);
+
+// console.log(p,c);
+// c.hello();
+// c.name = 'Hong';
+// c.age = '28';
+// c.hello();
+
+//static 상속
+//static변수도 정상 상속가능
+// class Parent{
+//     static age = 18;
+// }
+// class Child extends Parent{}
+// console.log(Parent.age,Child.age);
