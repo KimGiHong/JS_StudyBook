82745701dd1ada6e8685939c1e841875c5e673e1
김기홍 <kimgihong9@naver.com>
2021-01-13 23:43:48 +0900

    Promise

--- fastCampus/promise.js
+++ fastCampus/promise.js
@@ -46,11 +46,124 @@ p 라는 프로미스 객체는 1000ms 후에 fulfilled 상태가 됩ㄴ다.
 p 라는 프로미스 객체가 fulfilled 되는 시점에 p.then 안에 설정한 callback 함수가 실행됩니다.
 */
 
-const p = new Promise((resolve,reject) => {
-    /* pending */
-    setTimeout(() => {
-        resolve(); /* fulfiled */
-    }, 1000);
-})
+// const p = new Promise((resolve,reject) => {
+//     /* pending */
+//     setTimeout(() => {
+//         resolve(); /* fulfiled */
+//     }, 1000);
+// })
 
-p.then();
+// p.then();
+
+/*
+p.then으로 callback 함수를 설정했기 때문에 fulfilled 되면서 callback이 실행됩니다.
+*/
+
+// const p = new Promise((resolve,reject) => {
+//     /* pending */
+//     setTimeout(() => {
+//         resolve(); /* fulfiled */
+//     }, 1000);
+// })
+
+// p.then(() => {
+//     console.log('1000ms 후에 fulfilled 됩니다.');
+// });
+
+/*
+then을 설정하는 시점을 정확히하고,
+함수의 실행과 동시에 프로미스 객체를 만들면서 pending이 시작하도록 하기 위해
+프로미스 객체르르 생성하면서 리턴하는 함수 (p)를 만들어 함수 (p) 실행과 동시에 then을 생성
+ */
+
+// function p() {
+//      return new Promise((resolve,reject) => {
+//         /* pending */
+//         setTimeout(() => {
+//             resolve(); /* fulfiled */
+//         }, 1000);
+//     })
+//  }
+//  p().then(() => {
+//     console.log('1000ms 후에 fulfilled 됩니다.');
+//  });
+
+/*
+마찬가지로 프로미스 객체가 rejected 되는 시점에 p,catch 안에 설정한 callback 함수가 실행됩니다,
+*/
+
+// function p() {
+//      return new Promise((resolve,reject) => {
+//         /* pending */
+//         setTimeout(() => {
+//             reject(); /* rejected */
+//         }, 1000);
+//     })
+//  }
+
+// p().then(() => {
+//     console.log('1000ms 후에 fulfilled 됩니다.');
+//  }).catch(() => {
+//     console.log('1000ms 후에 rejected 됩니다.');
+//  });
+
+/*
+executor 의 resolve 함수를 실행할때 인자를 넣어 실행하면, then의 callback 함수의 인자로 받을 수 있습니다.
+    resolve('hello');
+    then((message) => {...})
+*/
+
+// function p() {
+//      return new Promise((resolve,reject) => {
+//         /* pending */
+//         setTimeout(() => {
+//             resolve('hello'); /* rejected */
+//         }, 1000);
+//     })
+//  }
+
+// p().then((message) => {
+//     console.log('1000ms 후에 fulfilled 됩니다.',message);
+//  }).catch(() => {
+//     console.log('1000ms 후에 rejected 됩니다.');
+//  });
+
+/*
+마찬가지로, executor의 reject 함수를 실행할때 인자를 넣어 실행하면, catch의 callback 함수의 인자로 받을 수 있다.
+    reject('error');
+    then((reson) => {...})
+*/
+
+// function p() {
+//      return new Promise((resolve,reject) => {
+//         /* pending */
+//         setTimeout(() => {
+//             reject('error'); /* rejected */
+//         }, 1000);
+//     })
+//  }
+
+// p().then((message) => {
+//     console.log('1000ms 후에 fulfilled 됩니다.',message);
+//  }).catch((reason) => {
+//     console.log('1000ms 후에 rejected 됩니다.',reason);
+//  });
+
+/* 
+보통 reject 함수를 실행하며 rejected 되는 이유를 넘기는데, 표준 내장 객체인 Error의 생성자를 이용하여 Error 객체를 만들 수 있다.
+*/
+
+// function p() {
+//     return new Promise((resolve,reject) => {
+//        /* pending */
+//        setTimeout(() => {
+//            reject(new Error('bad')); /* rejected */
+//        }, 1000);
+//    })
+// }
+
+// p().then((message) => {
+//    console.log('1000ms 후에 fulfilled 됩니다.',message);
+// }).catch((error) => {
+//    console.log('1000ms 후에 rejected 됩니다.',error);
+// });
